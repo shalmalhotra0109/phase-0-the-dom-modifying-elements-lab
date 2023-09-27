118ed69295c6a7a80cc4f50da8132d4d54a51f22
Shal Malhotra <smalhotra0109@gmail.com>
2023-09-26 21:11:51 -0400

    modifying elements changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById("main");
+// main.innerHTML = "<H1>Tigers</H1>";
+// const newHeader = document.querySelector("H1");
+
+// Move the newHeader variable assignment before removing the main element from the DOM
+
+main.parentNode.removeChild(main);
+
+const newHeader=document.createElement("H1")
+newHeader.setAttribute("id","victory");
+
+newHeader.textContent= "SHAL is the champion"
